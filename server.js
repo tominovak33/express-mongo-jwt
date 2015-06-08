712dc588949904328618447d5192ce0d14beee39
Tomi Novak <tominovak33@gmail.com>
2015-06-08 20:32:45 +0100

    Add bcrypt and password hashing

--- server.js
+++ server.js
@@ -3,30 +3,33 @@ var jwt = require('jwt-simple');
 var app = express();
 
 var _ = require('lodash');
+var bcrypt = require('bcrypt');
 
 app.use(require('body-parser').json());
 
 var secretkey = 'mysecretkey';
-var users = [{username: 'tomi' , password: 'pass'}]
+var users = [{username: 'tomi' , password: '$2a$10$C503Ai1d7CDQ8mvLSYhqsOcPLvZCezEu85ZfwpHsFLYvD4oVTvtAW'}]
 
 function findUserByUsername (username) {
 	return _.find(users, {username: username});
 }
 
-function validateUser (user, password) {
-	return user.password === password;
+function validateUser (user, password, callback) {
+	bcrypt.compare(password, user.password, callback);
+	//return user.password === password;
 }
 
 
 app.post('/session', function (request, response) {
 	var user = findUserByUsername(request.body.username);
-	if (!validateUser(user, request.body.password)) {
-		response.status(401);
-		return response.send("Authentication unsuccessful");
-	}
-
-	var token = jwt.encode({username: user.username}, secretkey);
-	response.json(token);
+	validateUser(user, request.body.password, function (error, valid) {
+		if (error || !valid) {
+			response.status(401);
+			return response.send("Authentication unsuccessful");
+		}
+		var token = jwt.encode({username: user.username}, secretkey);
+		response.json(token);
+	});
 })
 
 app.get('/user', function (request, response) {
